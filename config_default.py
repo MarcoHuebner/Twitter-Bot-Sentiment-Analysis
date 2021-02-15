246806d458dbbfd1817cf002cf7d92960ca2df4c
janelfes <janelfes@gmail.com>
2021-02-15 14:59:42 +0100

    possible way to make paths compatible via os

--- config_default.py
+++ config_default.py
@@ -4,7 +4,16 @@ Contains paths to:
 
 """
 
+import os
+
+abspath = os.path.abspath(__file__)
+work_dir = os.path.dirname(abspath)
+key_dir = work_dir + '/keys/keys.csv'
 
 class ConfigPaths(object):
     def __init__(self):
-        self.key_dir = "/path/to/keys.csv"
+        self.work_dir = work_dir
+        self.key_dir = key_dir
+        
+
+    
