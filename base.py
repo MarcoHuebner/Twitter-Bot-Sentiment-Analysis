fb3f70bace566dad9abf15b50c24e3bed9f2e521
MarcoHuebner <57489799+MarcoHuebner@users.noreply.github.com>
2021-02-18 22:03:50 +0100

    Minor optical changes

--- base.py
+++ base.py
@@ -60,14 +60,13 @@ ax.set_title('Common Words Found in Tweets')
 plt.show()
 
 # %%
-
+# Create bigrams and split them into letters (from only one row of df...?)
 terms_bigram = [list(bigrams(tweet)) for tweet in df['full_text']]
 bigrams = list(itertools.chain(*terms_bigram))
 
 # Create counter of words in clean bigrams
 bigram_counts = collections.Counter(bigrams)
-bigram_df = pd.DataFrame(bigram_counts.most_common(20),
-                         columns=['bigram', 'count'])
+bigram_df = pd.DataFrame(bigram_counts.most_common(20), columns=['bigram', 'count'])
 
 # Create dictionary of bigrams and their counts
 d = bigram_df.set_index('bigram').T.to_dict('records')
@@ -80,28 +79,16 @@ for k, v in d[0].items():
     G.add_edge(k[0], k[1], weight=(v * 10))
 
 G.add_node("china", weight=100)
-
 fig, ax = plt.subplots(figsize=(10, 8))
-
 pos = nx.spring_layout(G, k=2)
 
 # Plot networks
-nx.draw_networkx(G, pos,
-                 font_size=16,
-                 width=3,
-                 edge_color='grey',
-                 node_color='purple',
-                 with_labels=False,
-                 ax=ax)
+nx.draw_networkx(G, pos, font_size=16, width=3, edge_color='grey', node_color='purple', with_labels=False, ax=ax)
 
 # Create offset labels
 for key, value in pos.items():
     x, y = value[0]+.135, value[1]+.045
-    ax.text(x, y,
-            s=key,
-            bbox=dict(facecolor='red', alpha=0.25),
-            horizontalalignment='center', fontsize=13)
-
+    ax.text(x, y, s=key, bbox=dict(facecolor='red', alpha=0.25), horizontalalignment='center', fontsize=13)
 plt.show()
 
 # %%
