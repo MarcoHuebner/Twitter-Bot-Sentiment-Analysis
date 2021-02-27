d216ea0d27ce8c63a09c0f19aba4d2b81c20d3c7
MarcoHuebner <57489799+MarcoHuebner@users.noreply.github.com>
2021-02-27 19:49:50 +0100

    Updated event impact
    moved data_transform, added TODO for sentiment analysis. Will check outcomes/ make the change myself tomorrow and check the theory behind given methods

--- event_impact_sentiment.py
+++ event_impact_sentiment.py
@@ -1,38 +1,38 @@
-import itertools
+"""
+# TODO: Description
+
+"""
+
 import datetime
-
-import matplotlib.pyplot as plt
+import itertools
 import pandas as pd
+import matplotlib.pyplot as plt
 
-# from textblob import TextBlob
-from textblob_de import TextBlobDE as TextBlob
-from helpers import Helpers
 from scipy import stats
+from textblob_de import TextBlobDE as TextBlob
+# from textblob import TextBlob
+
+from helpers import Helpers
+
 
 # initialize api
 api_helpers = Helpers()
 
-
-def date_transform(date: str, format: str = '%a %b %d %X %z %Y') -> datetime:
-    '''
-    transforms date in datetime
-    :param date: str, date to be transform into datetime object
-    :return: datetime
-    '''
-    return datetime.datetime.strptime(date, format)
-
-
+# load and preprocess data
 df = api_helpers.data_handler(tweets=None, geo=None, user_metadata=True, from_cursor=False,
                               filename="example_search.txt")
-api_helpers.clean_text_df(df)
-df['date'] = df['date'].transform(date_transform).sort_values()
+trans_df = api_helpers.clean_text_df(df)
 
+
+# %% Sentiment analysis conditioned on cut-off date
+# set cut-off date
 cutoff_date = datetime.datetime.strptime('Feb 24 08:00:00 +0000 2021', '%b %d %X %z %Y')
 df['cond'] = df['date'] >= cutoff_date
 
 df_past, df_pre = df.loc[df['cond']], df.loc[~df['cond']]
 
 # Sentiment analysis past cutoff_date
+# TODO: Sentiment analysis function in helpers (conditioned on date/ data to avoid repetition)
 sentiment_objects_past = [[TextBlob(word) for word in tweet] for tweet in df_past['text']]
 sentiment_values_past = [[[word.sentiment.polarity, str(word)] for word in tweet] for tweet in sentiment_objects_past]
 sentiment_df_past = pd.DataFrame(list(itertools.chain(*sentiment_values_past)), columns=["polarity", "tweet"])
@@ -41,6 +41,7 @@ sentiment_df_past = pd.DataFrame(list(itertools.chain(*sentiment_values_past)), 
 sentiment_objects_pre = [[TextBlob(word) for word in tweet] for tweet in df_pre['text']]
 sentiment_values_pre = [[[word.sentiment.polarity, str(word)] for word in tweet] for tweet in sentiment_objects_pre]
 sentiment_df_pre = pd.DataFrame(list(itertools.chain(*sentiment_values_pre)), columns=["polarity", "tweet"])
+
 
 # %% plot sentiments (without neutral words)
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 9))
@@ -55,6 +56,7 @@ ax2.set_xlabel('sentiment value')
 ax2.set_yscale('log')
 
 plt.show()
-# %% t test
 
+
+# %% t test
 tstat, pval = stats.ttest_ind(sentiment_df_past['polarity'], sentiment_df_pre['polarity'])
